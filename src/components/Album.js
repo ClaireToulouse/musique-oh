637402cf18075fa7dec6ab38db56a0ea52910bda
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-19 13:39:15 +0200

    responsiveavec colonnes sur App et Album

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,7 +3,7 @@ import { Media } from 'reactstrap';
 
 const Album = ({cover, title, artist}) => {
   return (
-    <Media className="py-2">
+    <Media className="py-2 col-9">
       <Media left href="#">
         <Media src={cover} alt={`photo cover de ${title}`}/>
       </Media>
